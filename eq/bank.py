af780daa6adfc30d8e63e11d81199a79359c3dda
laura.panzariello <laura.panzariello@corp.globo.com>
2020-10-03 21:54:49 -0300

    __eq__ method

---
+++ eq/bank.py
@@ -0,0 +1,35 @@
+class Account:
+
+    def __init__(self, account):
+        self._account = account
+        self._balance = 0
+
+    def deposit(self, value):
+        self._balance += value
+
+    def __str__(self):
+        return "[>> Account {} Balance {}<<]".format(self._account, self._balance)
+
+
+if __name__ == '__main__':
+    c1 = Account(10)
+    c2 = Account(10)
+
+    print("Without the __eq__ method")
+
+    accounts = [c1]
+
+    if c1 in accounts:
+        print("c1 is in accounts")
+    else:
+        print("c1 is not in accounts")
+
+    if c2 in accounts:
+        print("c2 is in accounts")
+    else:
+        print("c2 is not in accounts")
+
+    if c1 == c2:
+        print("c1 == c2")
+    else:
+        print("c1 != c2")
